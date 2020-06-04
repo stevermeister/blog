2b5e5be6076412a54076d5f229af872cb500ec28
Stepan Suvorov <stevermeister@gmail.com>
2020-06-04 14:18:11 +0200

    tags

--- src/app/tag.service.ts
+++ src/app/tag.service.ts
@@ -42,48 +42,7 @@ export class TagService {
           }
         });
       });
-      return tags;
+      return tags.filter(tag => tag.count > 3);
     }));
   }
 }
-
-/*
-tags = [
-  { title: 'Angular', count: 84},
-  { title: 'browser', count: 6},
-  { title: 'chrome', count: 16},
-  { title: 'console', count: 11},
-  { title: 'debug', count: 8},
-  { title: 'design', count: 4},
-  { title: 'event', count: 4},
-  { title: 'facebook', count: 4},
-  { title: 'fileapi', count: 4},
-  { title: 'git', count: 14},
-  { title: 'GitHub', count: 8},
-  { title: 'google', count: 7},
-  { title: 'grunt', count: 7},
-  { title: 'html5', count: 6},
-  { title: 'idea', count: 5},
-  { title: 'javascript', count: 73},
-  { title: 'jQuery', count: 14},
-  { title: 'mac', count: 12},
-  { title: 'MongoDB', count: 5},
-  { title: 'mysql', count: 6},
-  { title: 'node.js', count: 16},
-  { title: 'npm', count: 5},
-  { title: 'online', count: 45},
-  { title: 'php', count: 8},
-  { title: 'phpStorm', count: 4},
-  { title: 'protractor', count: 4},
-  { title: 'quiz', count: 5},
-  { title: 'shell', count: 4},
-  { title: 'skype', count: 6},
-  { title: 'sql', count: 5},
-  { title: 'terminal', count: 5},
-  { title: 'tests', count: 5},
-  { title: 'tool', count: 30},
-  { title: 'ubuntu', count: 21},
-  { title: 'webStorm', count: 9},
-  { title: 'отзывы', count: 5},
-  { title: 'собеседование', count: 6}
-];*/
